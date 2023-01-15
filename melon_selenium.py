055c751bbd1a59d1ce2c2210287abb7a26485f84
kkwon <soonbin1997@gmail.com>
2023-01-15 23:50:38 +0900

    modify

--- melon_selenium.py
+++ melon_selenium.py
@@ -22,11 +22,11 @@ sql = "INSERT INTO melon_top_100 (title, singer, album, likes) VALUES (%s, %s, %
 with conn:
     with conn.cursor() as cur:
         for song in (song_parts):
-            title = song.find('div', class_= 'ellipsis rank01').get_text().replace('\n', '')
             singer = song.find('span', class_= 'checkEllipsis').get_text().replace('\n', '')
+            title = song.find('div', class_= 'ellipsis rank01').get_text().replace('\n', '')
             album = song.find('div', class_= 'ellipsis rank03').get_text().replace('\n', '').replace("'", "\'")
-            likes = song.find('button', class_ = 'button_etc like').get_text().replace('\n', '').replace('좋아요총건수', '').replace(',', '')
-            cur.execute(sql, (title, singer, album, likes))
+            like = song.find('button', class_ = 'button_etc like').get_text().replace('\n', '').replace('좋아요총건수', '').replace(',', '')
+            cur.execute(sql, (title, singer, album, like))
             conn.commit()
 
 driver.quit()
